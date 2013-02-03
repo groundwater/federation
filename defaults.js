5630680b99b9614a7106eb8dc77dce5fcf948a52
Jacob Groundwater <groundwater@gmail.com>
2013-02-03 13:19:44 -0800

    Add HTTP Comments to defaults

--- defaults.js
+++ defaults.js
@@ -10,8 +10,11 @@ var defaults = {
   // To disable a transport, assign the `disabled` key, or remove the option altogether.
   transports: {
     
-    // Axon Transports
+    // Axon Transport Options
     axon: {
+      
+      // To disable axon transport, un-comment the following line
+      // disabled: true,
       
       // The location of the module to load
       // Relative paths are resolved relative to the federation module 
@@ -25,14 +28,23 @@ var defaults = {
       PORT_CONNECT: 8973
     },
     
+    // HTTP Transport Options
     http: {
       
+      // HTTP transport is disabled by default
+      // Remove the following key to enable
       disabled: true,
       
       module : 'transports/http',
       
+      // You can make this port 80, but that will require running
+      // your service as root. We do not recommend such crazyness.
+      //
+      // The default HTTP transport is proxy-friendly, so you should
+      // be able to situate this behind an HA-Proxy or NGINX.
       PORT_BIND: 8974,
       
+      // The default HTTP port to connect to if none is specified.
       PORT_CONNECT: 8974
       
     }
